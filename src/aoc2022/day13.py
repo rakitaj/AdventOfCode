ddb346a25c63a6d43914c83cb5e3e6a0b1c91406
Joshua Rakita <joraki@microsoft.com>
2023-02-21 18:43:47 -0800

    Different parsing for day 13

--- src/aoc2022/day13.py
+++ src/aoc2022/day13.py
@@ -1,21 +1,41 @@
 from __future__ import annotations
+from typing import Any
 import ast
 
-# class NestedList:
-#     def __init__(self, parent: NestedList | None, children: list[NestedList] | None, data: list[int]):
-#         self.parent = parent
-#         self.child = children
-#         self.data = data
+
+# [[1],[2,3,4]]
+# [[1],4]
+
+# [9]
+# [[8,7,6]]
+
+# [[4,4],4,4]
+# [[4,4],4,4,4]
 
 
-# def parse_line(line: str) -> NestedList:
-#     root = NestedList(None, None, list())
-#     while i := 0 < len(line):
-#         if line[i] == "[":
-#             i += i
-#     return root
+class NestedList:
+    def __init__(self, parent: NestedList | None | None, data: list[int]):
+        self.parent = parent
+        self.data = data
 
 
-def parse_line(line: str) -> ast.AST:
+def parse_line_2(line: str) -> NestedList:
+    root = NestedList(None, list())
+    current = root
+    i = 0
+    while i < len(line):
+        if line[i] == "[":
+            child_list = NestedList(current, list())
+            current = child_list
+        elif line[i] == "]" and current.parent is not None:
+            current = current.parent
+        else:
+            number = ""
+            while line[i].
+        i += i
+    return root
+
+
+def parse_line(line: str) -> Any:
     x = ast.literal_eval(line)
     return x
