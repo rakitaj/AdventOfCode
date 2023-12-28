481d511698adb8d8dd81fef9e0fa5d22c1b887c7
Joshua Rakita <joraki@microsoft.com>
2023-12-28 13:15:02 -0800

    Working on 2023-03.1

--- src/aoc2023/day03.py
+++ src/aoc2023/day03.py
@@ -1,2 +1,68 @@
+from typing import Sequence
 from src.common.dataload import DataLoader, Answers
-from src.common.grid import Grid
+import re
+
+pattern_numbers = re.compile(r"(\d+)")
+
+
+def symbol_adjacent(num_start_end: tuple[int, int], symbols: list[int]) -> bool:
+    """Is the number adjacent to any symbols?
+    Range (3, 7)
+    Symbols [2, 10]
+    """
+    start, end = num_start_end
+    for i in symbols:
+        if i >= start - 1 and i <= end + 1:
+            return True
+    return False
+
+
+def match_symbols(line: str) -> list[int]:
+    mactch_indices = list()
+    for i, e in enumerate(line):
+        if e != "." and not e.isdigit() and e != "\n":
+            mactch_indices.append(i)
+    return mactch_indices
+
+
+def part_numbers(prev_line: str | None, line: str, next_line: str | None) -> list[int]:
+    numbers: list[int] = list()
+    symbol_matches: list[int] = list()
+    for l in (prev_line, line, next_line):
+        if l is not None:
+            match_indices = match_symbols(l)
+            symbol_matches.extend(match_indices)
+    for m in pattern_numbers.finditer(line):
+        if symbol_adjacent((m.start(), m.end() - 1), symbol_matches):
+            numbers.append(int(m.group(1)))
+    return numbers
+
+
+def part_numbers_all_lines(lines: Sequence[str]) -> int:
+    total = list()
+    for i in range(len(lines)):
+        if i - 1 < 0:
+            prev_line = None
+        else:
+            prev_line = lines[i - 1]
+        if i + 1 >= len(lines):
+            next_line = None
+        else:
+            next_line = lines[i + 1]
+        nums = part_numbers(prev_line, lines[i], next_line)
+        total.extend(nums)
+    return sum(total)
+
+
+class Day03Answers(Answers):
+    def __init__(self):
+        loader = DataLoader(2023, "day03.txt")
+        self.data = loader.readlines_str()
+
+    def part1(self) -> str:
+        """Too high 88151870"""
+        total = part_numbers_all_lines(self.data)
+        return str(total)
+
+    def part2(self) -> str:
+        return "Not yet"
---
+++ src/tests/tests2023/test_day03.py
@@ -0,0 +1,55 @@
+import pytest
+from src.aoc2023.day03 import part_numbers, symbol_adjacent, part_numbers_all_lines
+
+sample_data = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""".splitlines()
+
+sample_data_long = """...788.............................54.........501...........555.........270.................................521......893....................
+..../..*963........................*..860......................*....53...../.....................52.................&....347........428*522.""".splitlines()
+
+sample_data_2 = """12.......*..
++.........34
+.......-12..
+..78........
+..*....60...
+78.........9
+.5.....23..$
+8...90*12...
+............
+2.2......12.
+.*.........*
+1.1..503+.56""".splitlines()
+
+
+def test_part_numers_with_sample_data():
+    numbers = part_numbers(None, sample_data[0], sample_data[1])
+    assert numbers == [467]
+
+
+@pytest.mark.parametrize("num_range, symbols, expected", [((3, 7), [2, 10], True)])
+def test_symbol_adjacent(num_range: tuple[int, int], symbols: list[int], expected: bool):
+    assert symbol_adjacent(num_range, symbols) == expected
+
+
+@pytest.mark.parametrize("data_var, expected", [(sample_data, 4361), (sample_data_2, 925)])
+def test_part_numbers_sample_data(data_var: list[str], expected: int):
+    actual = part_numbers_all_lines(data_var)
+    assert actual == expected
+
+
+def test_part_numbers_sample_data_long():
+    actual = part_numbers_all_lines(sample_data_long)
+    assert actual == sum([788, 54, 555, 270, 893, 963, 428, 522])
+
+
+def test_part_numbers_eol_does_not_count():
+    actual = part_numbers_all_lines(["123..........45*..6\n"])
+    assert actual == 45
