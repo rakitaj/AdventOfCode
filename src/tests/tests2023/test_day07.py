75be0998ef21a3c3a75106b118df2a8fabd0140c
Joshua Rakita <joraki@microsoft.com>
2023-12-29 15:56:00 -0800

    Working on 2023-07.1

---
+++ src/tests/tests2023/test_day07.py
@@ -0,0 +1,13 @@
+import pytest
+from src.aoc2023.day07 import compare_hands, Hand, HandType
+
+
+@pytest.mark.parametrize(
+    "hand1, hand2, expected",
+    [
+        (Hand(HandType.PAIR_2, "23432"), Hand(HandType.PAIR_2, "23432"), 0),
+        (Hand(HandType.FULL_HOUSE, "KK888"), Hand(HandType.KIND_3, "44423"), 1),
+    ],
+)
+def test_compare_hands(hand1: Hand, hand2: Hand, expected: int):
+    assert compare_hands(hand1, hand2) == expected
