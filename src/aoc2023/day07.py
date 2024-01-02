04121f39541fa85418415343536dd3d81a175b16
Joshua Rakita <joshuarakita@gmail.com>
2024-01-02 12:50:33 -0800

    Done with 2023-07.1

--- src/aoc2023/day07.py
+++ src/aoc2023/day07.py
@@ -1,5 +1,9 @@
+from typing import Sequence
 from enum import IntEnum
 from dataclasses import dataclass
+from collections import Counter
+from functools import cmp_to_key
+from src.common.dataload import DataLoader, Answers
 
 
 class HandType(IntEnum):
@@ -32,6 +36,7 @@ def card_rank(rank: str) -> int:
 class Hand:
     hand_type: HandType
     contents: str
+    bid: int
 
 
 def inner_hand_sort(hand1: Hand, hand2: Hand) -> int:
@@ -55,3 +60,59 @@ def compare_hands(hand1: Hand, hand2: Hand) -> int:
         return inner_hand_sort(hand1, hand2)
     else:
         return hand1.hand_type - hand2.hand_type
+
+
+def parse_to_hand(line: str) -> Hand:
+    hand_str, bid = line.split()
+    counts = Counter(hand_str)
+    hand_type = HandType.HIGH_CARD
+    if 5 in counts.values():
+        hand_type = HandType.KIND_5
+    elif 4 in counts.values():
+        hand_type = HandType.KIND_4
+    elif 3 in counts.values() and 2 in counts.values():
+        hand_type = HandType.FULL_HOUSE
+    elif 3 in counts.values():
+        hand_type = HandType.KIND_3
+    elif 2 in counts.values():
+        pair_count = 0
+        for val in counts.values():
+            if val == 2:
+                pair_count += 1
+        if pair_count == 1:
+            hand_type = HandType.PAIR_1
+        if pair_count == 2:
+            hand_type = HandType.PAIR_2
+    else:
+        hand_type = HandType.HIGH_CARD
+    return Hand(hand_type, hand_str, int(bid))
+
+
+def parse_many_hands(lines: Sequence[str]) -> list[Hand]:
+    hands: list[Hand] = list()
+    for line in lines:
+        hand = parse_to_hand(line)
+        hands.append(hand)
+    return hands
+
+
+def calcuate_part1_answer(lines: Sequence[str]) -> int:
+    hands = parse_many_hands(lines)
+    sorted_hands = sorted(hands, key=cmp_to_key(compare_hands))
+    total = 0
+    for i, hand in enumerate(sorted_hands):
+        total += hand.bid * (i + 1)
+    return total
+
+
+class Day07Answers(Answers):
+    def __init__(self):
+        loader = DataLoader(2023, "day07.txt")
+        self.data = loader.readlines_str()
+
+    def part1(self) -> int:
+        answer = calcuate_part1_answer(self.data)
+        return answer
+
+    def part2(self) -> int:
+        return -1
--- src/tests/tests2023/test_day07.py
+++ src/tests/tests2023/test_day07.py
@@ -1,15 +1,33 @@
 import pytest
-from src.aoc2023.day07 import compare_hands, Hand, HandType
+from src.aoc2023.day07 import compare_hands, Hand, HandType, parse_to_hand, calcuate_part1_answer
 
 
 @pytest.mark.parametrize(
     "hand1, hand2, expected",
     [
-        (Hand(HandType.PAIR_2, "23432"), Hand(HandType.PAIR_2, "23432"), 0),
-        (Hand(HandType.FULL_HOUSE, "KK888"), Hand(HandType.KIND_3, "44423"), 1),
-        (Hand(HandType.PAIR_1, "QQ567"), Hand(HandType.PAIR_1, "QQ567"), 0),
-        (Hand(HandType.PAIR_1, "QQ565"), Hand(HandType.PAIR_1, "QQ567"), -1),
+        (Hand(HandType.PAIR_2, "23432", 28), Hand(HandType.PAIR_2, "23432", 28), 0),
+        (Hand(HandType.FULL_HOUSE, "KK888", 100), Hand(HandType.KIND_3, "44423", 200), 1),
+        (Hand(HandType.PAIR_1, "QQ567", 123), Hand(HandType.PAIR_1, "QQ567", 123), 0),
+        (Hand(HandType.PAIR_1, "QQ565", 321), Hand(HandType.PAIR_1, "QQ567", 509), -1),
     ],
 )
 def test_compare_hands(hand1: Hand, hand2: Hand, expected: int):
     assert compare_hands(hand1, hand2) == expected
+
+
+@pytest.mark.parametrize(
+    "hand_str, expected",
+    [("23432 101", Hand(HandType.PAIR_2, "23432", 101)), ("AAAAA 28", Hand(HandType.KIND_5, "AAAAA", 28))],
+)
+def test_parse_to_hand(hand_str: str, expected: Hand):
+    assert parse_to_hand(hand_str) == expected
+
+
+def test_compute_part1_answer():
+    lines = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""".splitlines()
+    answer = calcuate_part1_answer(lines)
+    assert answer == 6440
