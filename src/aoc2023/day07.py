ec010e27037148c40d06c3c6d931d77cff33d907
Joshua Rakita <joshuarakita@gmail.com>
2024-01-02 23:20:33 -0800

    Working on 2023-07.2

--- src/aoc2023/day07.py
+++ src/aoc2023/day07.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Callable
 from enum import IntEnum
 from dataclasses import dataclass
 from collections import Counter
@@ -16,20 +16,8 @@ class HandType(IntEnum):
     HIGH_CARD = 1
 
 
-def card_rank(rank: str) -> int:
-    match rank:
-        case "A":
-            return 14
-        case "K":
-            return 13
-        case "Q":
-            return 12
-        case "J":
-            return 11
-        case "T":
-            return 10
-        case _:
-            return int(rank)
+CARD_RANK = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+CARD_RANK_2 = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 
 @dataclass
@@ -39,12 +27,12 @@ class Hand:
     bid: int
 
 
-def inner_hand_sort(hand1: Hand, hand2: Hand) -> int:
+def inner_hand_sort(rank_array: list[str], hand1: Hand, hand2: Hand) -> int:
     assert hand1.hand_type == hand2.hand_type
-    for i in range(len(hand1.contents)):
-        if card_rank(hand1.contents[i]) > card_rank(hand2.contents[i]):
+    for i, e in enumerate(hand1.contents):
+        if rank_array.index(hand1.contents[i]) > rank_array.index(hand2.contents[i]):
             return 1
-        if card_rank(hand2.contents[i]) > card_rank(hand1.contents[i]):
+        if rank_array.index(hand2.contents[i]) > rank_array.index(hand1.contents[i]):
             return -1
     return 0
 
@@ -57,7 +45,20 @@ def compare_hands(hand1: Hand, hand2: Hand) -> int:
     1  -> hand1 > hand2
     """
     if hand1.hand_type == hand2.hand_type:
-        return inner_hand_sort(hand1, hand2)
+        return inner_hand_sort(CARD_RANK, hand1, hand2)
+    else:
+        return hand1.hand_type - hand2.hand_type
+
+
+def compare_hands_2(hand1: Hand, hand2: Hand) -> int:
+    """
+    Compare two hands.
+    -1 -> hand1 < hand2.
+    0  -> hand1 == hand2.
+    1  -> hand1 > hand2
+    """
+    if hand1.hand_type == hand2.hand_type:
+        return inner_hand_sort(CARD_RANK_2, hand1, hand2)
     else:
         return hand1.hand_type - hand2.hand_type
 
@@ -88,17 +89,55 @@ def parse_to_hand(line: str) -> Hand:
     return Hand(hand_type, hand_str, int(bid))
 
 
-def parse_many_hands(lines: Sequence[str]) -> list[Hand]:
+def parse_to_hand_2(line: str) -> Hand:
+    hand_str, bid = line.split()
+    counts = Counter(hand_str)
+    hand_type = HandType.HIGH_CARD
+    jack_count = counts["J"]
+    if 5 in counts.values():
+        hand_type = HandType.KIND_5
+    elif 4 in counts.values():
+        hand_type = HandType.KIND_4
+        if jack_count in [1, 4]:
+            hand_type = HandType.KIND_5
+    elif 3 in counts.values() and 2 in counts.values():
+        hand_type = HandType.FULL_HOUSE
+        if jack_count in [2, 3]:
+            hand_type = HandType.KIND_5
+    elif 3 in counts.values():
+        hand_type = HandType.KIND_3
+        if jack_count in [1, 3]:
+            hand_type = HandType.KIND_4
+    elif 2 in counts.values():
+        pair_count = 0
+        for val in counts.values():
+            if val == 2:
+                pair_count += 1
+        if pair_count == 1:
+            hand_type = HandType.PAIR_1
+            if jack_count == 1:
+                hand_type = HandType.KIND_3
+        if pair_count == 2:
+            hand_type = HandType.PAIR_2
+            if jack_count == 1:
+                hand_type = HandType.FULL_HOUSE
+            if jack_count == 2:
+                hand_type = HandType.KIND_4
+    else:
+        hand_type = HandType.HIGH_CARD
+    return Hand(hand_type, hand_str, int(bid))
+
+
+def calcuate_answer(
+    lines: Sequence[str],
+    parse_func: Callable[[str], Hand],
+    compare_func: Callable[[Hand, Hand], int],
+) -> int:
     hands: list[Hand] = list()
     for line in lines:
-        hand = parse_to_hand(line)
+        hand = parse_func(line)
         hands.append(hand)
-    return hands
-
-
-def calcuate_part1_answer(lines: Sequence[str]) -> int:
-    hands = parse_many_hands(lines)
-    sorted_hands = sorted(hands, key=cmp_to_key(compare_hands))
+    sorted_hands = sorted(hands, key=cmp_to_key(compare_func))
     total = 0
     for i, hand in enumerate(sorted_hands):
         total += hand.bid * (i + 1)
@@ -111,8 +150,9 @@ class Day07Answers(Answers):
         self.data = loader.readlines_str()
 
     def part1(self) -> int:
-        answer = calcuate_part1_answer(self.data)
+        answer = calcuate_answer(self.data, parse_to_hand, compare_hands)
         return answer
 
     def part2(self) -> int:
-        return -1
+        answer = calcuate_answer(self.data, parse_to_hand_2, compare_hands_2)
+        return answer
--- src/tests/tests2023/test_day07.py
+++ src/tests/tests2023/test_day07.py
@@ -1,5 +1,13 @@
 import pytest
-from src.aoc2023.day07 import compare_hands, Hand, HandType, parse_to_hand, calcuate_part1_answer
+from src.aoc2023.day07 import (
+    compare_hands,
+    Hand,
+    HandType,
+    parse_to_hand,
+    calcuate_answer,
+    parse_to_hand_2,
+    compare_hands_2,
+)
 
 
 @pytest.mark.parametrize(
@@ -9,6 +17,7 @@ from src.aoc2023.day07 import compare_hands, Hand, HandType, parse_to_hand, calc
         (Hand(HandType.FULL_HOUSE, "KK888", 100), Hand(HandType.KIND_3, "44423", 200), 1),
         (Hand(HandType.PAIR_1, "QQ567", 123), Hand(HandType.PAIR_1, "QQ567", 123), 0),
         (Hand(HandType.PAIR_1, "QQ565", 321), Hand(HandType.PAIR_1, "QQ567", 509), -1),
+        (Hand(HandType.KIND_4, "J5555", 555), Hand(HandType.KIND_4, "T5555", 555), 1),
     ],
 )
 def test_compare_hands(hand1: Hand, hand2: Hand, expected: int):
@@ -29,5 +38,22 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483""".splitlines()
-    answer = calcuate_part1_answer(lines)
+    answer = calcuate_answer(lines, parse_func=parse_to_hand, compare_func=compare_hands)
     assert answer == 6440
+
+
+def test_compute_part2_answer():
+    lines = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483""".splitlines()
+    answer = calcuate_answer(lines, parse_func=parse_to_hand_2, compare_func=compare_hands_2)
+    assert answer == 5905
+
+
+def test_compute_part2_answer_2():
+    lines = """JAAKK 1
+JJJAK 2""".splitlines()
+    answer = calcuate_answer(lines, parse_func=parse_to_hand_2, compare_func=compare_hands_2)
+    assert answer == 5
