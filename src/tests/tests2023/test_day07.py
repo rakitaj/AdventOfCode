4cee19eb26f485a5bf1a809eee1a906a59d47761
Joshua Rakita <joraki@microsoft.com>
2024-01-04 09:11:33 -0800

    Used object based sorting instead of a callback to sort the hands.

--- src/tests/tests2023/test_day07.py
+++ src/tests/tests2023/test_day07.py
@@ -1,32 +1,60 @@
 import pytest
 from src.aoc2023.day07 import (
-    compare_hands,
     Hand,
     HandType,
     parse_to_hand,
     calcuate_answer,
     parse_to_hand_2,
-    compare_hands_2,
 )
+
+part_1_strategy = lambda x: x.CARD_RANK
 
 
 @pytest.mark.parametrize(
     "hand1, hand2, expected",
     [
-        (Hand(HandType.PAIR_2, "23432", 28), Hand(HandType.PAIR_2, "23432", 28), 0),
-        (Hand(HandType.FULL_HOUSE, "KK888", 100), Hand(HandType.KIND_3, "44423", 200), 1),
-        (Hand(HandType.PAIR_1, "QQ567", 123), Hand(HandType.PAIR_1, "QQ567", 123), 0),
-        (Hand(HandType.PAIR_1, "QQ565", 321), Hand(HandType.PAIR_1, "QQ567", 509), -1),
-        (Hand(HandType.KIND_4, "J5555", 555), Hand(HandType.KIND_4, "T5555", 555), 1),
+        (
+            Hand(HandType.PAIR_2, "23432", 28, part_1_strategy),
+            Hand(HandType.PAIR_2, "23432", 28, part_1_strategy),
+            False,
+        ),
+        (
+            Hand(HandType.FULL_HOUSE, "KK888", 100, part_1_strategy),
+            Hand(HandType.KIND_3, "44423", 200, part_1_strategy),
+            False,
+        ),
+        (
+            Hand(HandType.PAIR_1, "QQ567", 123, part_1_strategy),
+            Hand(HandType.PAIR_1, "QQ567", 123, part_1_strategy),
+            False,
+        ),
+        (
+            Hand(HandType.PAIR_1, "QQ565", 321, part_1_strategy),
+            Hand(HandType.PAIR_1, "QQ567", 509, part_1_strategy),
+            True,
+        ),
+        (
+            Hand(HandType.KIND_4, "J5555", 555, part_1_strategy),
+            Hand(HandType.KIND_4, "T5555", 555, part_1_strategy),
+            False,
+        ),
+        (
+            Hand(HandType.KIND_4, "52TJJ", 555, part_1_strategy),
+            Hand(HandType.KIND_4, "52TKK", 555, part_1_strategy),
+            True,
+        ),
     ],
 )
-def test_compare_hands(hand1: Hand, hand2: Hand, expected: int):
-    assert compare_hands(hand1, hand2) == expected
+def test_compare_hands(hand1: Hand, hand2: Hand, expected: bool):
+    assert (hand1 < hand2) == expected
 
 
 @pytest.mark.parametrize(
     "hand_str, expected",
-    [("23432 101", Hand(HandType.PAIR_2, "23432", 101)), ("AAAAA 28", Hand(HandType.KIND_5, "AAAAA", 28))],
+    [
+        ("23432 101", Hand(HandType.PAIR_2, "23432", 101, part_1_strategy)),
+        ("AAAAA 28", Hand(HandType.KIND_5, "AAAAA", 28, part_1_strategy)),
+    ],
 )
 def test_parse_to_hand(hand_str: str, expected: Hand):
     assert parse_to_hand(hand_str) == expected
@@ -38,7 +66,7 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483""".splitlines()
-    answer = calcuate_answer(lines, parse_func=parse_to_hand, compare_func=compare_hands)
+    answer = calcuate_answer(lines, parse_func=parse_to_hand)
     assert answer == 6440
 
 
@@ -48,22 +76,22 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483""".splitlines()
-    answer = calcuate_answer(lines, parse_func=parse_to_hand_2, compare_func=compare_hands_2)
+    answer = calcuate_answer(lines, parse_func=parse_to_hand_2)
     assert answer == 5905
 
 
 def test_compute_part2_answer_2():
     lines = """JAAKK 1
 JJJAK 2""".splitlines()
-    answer = calcuate_answer(lines, parse_func=parse_to_hand_2, compare_func=compare_hands_2)
+    answer = calcuate_answer(lines, parse_func=parse_to_hand_2)
     assert answer == 5
 
 
 @pytest.mark.parametrize(
-    "parse_func, compare_func, expected",
-    [(parse_to_hand, compare_hands, 6592), (parse_to_hand_2, compare_hands_2, 6839)],
+    "parse_func, expected",
+    [(parse_to_hand, 6592), (parse_to_hand_2, 6839)],
 )
-def test_compute_both_parts(parse_func, compare_func, expected: int):
+def test_compute_both_parts(parse_func, expected: int):
     lines = """2345A 1
 Q2KJJ 13
 Q2Q2Q 19
@@ -83,5 +111,5 @@ AAAAA 61
 2AAAA 23
 2JJJJ 53
 JJJJ2 41""".splitlines()
-    answer = calcuate_answer(lines, parse_func=parse_func, compare_func=compare_func)
+    answer = calcuate_answer(lines, parse_func=parse_func)
     assert answer == expected
